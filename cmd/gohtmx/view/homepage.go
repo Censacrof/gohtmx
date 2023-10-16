43804c812ce7b1fb0ce5039420c1b67109766dc7
Francesco Galisi <galisifrancesco@gmail.com>
2023-10-17 00:40:21 +0200

    Add getUserListEntryPage

--- cmd/gohtmx/view/homepage.go
+++ cmd/gohtmx/view/homepage.go
@@ -1,21 +1,33 @@
 package view
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/Censacrof/gohtmx/cmd/gohtmx/components"
 )
 
+func getUserListEntriesPage(from int, size int) template.HTML {
+	var entries template.HTML
+
+	for i := 0; i < size; i++ {
+		avatar := template.URL(fmt.Sprintf("https://robohash.org/%d", i))
+		entries += components.UserListEntry{
+			Avatar: avatar,
+		}.HTML()
+	}
+	return entries
+}
+
 func Homepage(w http.ResponseWriter, r *http.Request) {
+	intial_entries := getUserListEntriesPage(0, 10)
+
 	var body = components.BasePage{
 		Title: "Homepage",
 		Body: components.Wrapper{
 			Content: components.UserList{
-				Entries: components.UserListEntry{
-					Avatar: "https://robohash.org/0",
-				}.HTML() + components.UserListEntry{
-					Avatar: "https://robohash.org/1",
-				}.HTML(),
+				Entries: intial_entries,
 			}.HTML(),
 		}.HTML(),
 	}.HTML()
