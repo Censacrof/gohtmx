bc8968c0499423b9780f29c742fb6e189adf0500
Francesco Galisi <galisifrancesco@gmail.com>
2023-10-22 01:22:34 +0200

    Experiment with components composition

--- cmd/gohtmx/main.go
+++ cmd/gohtmx/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/Censacrof/gohtmx/cmd/gohtmx/templateloader"
 	"github.com/a-h/templ"
 )
 
@@ -14,25 +13,15 @@ const PORT = 8000
 func main() {
 	fmt.Printf("Server started on http://localhost:%d/\n", PORT)
 
-	component := hello("John")
-
-	http.Handle("/", templ.Handler(component))
-
 	// static files
-	// fs := http.FileServer(http.Dir("./web/static"))
-	// http.Handle("/static/", http.StripPrefix("/static", fs))
+	fs := http.FileServer(http.Dir("./web/static"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
 
-	// // hot reload
-	// http.HandleFunc("/_hotreloadws", hotreload)
+	// hot reload
+	http.HandleFunc("/_hotreloadws", hotreload)
 
 	// // // views
-	// http.HandleFunc("/", Homepage)
+	http.Handle("/", templ.Handler(HomePage()))
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
-}
-
-func Homepage(w http.ResponseWriter, r *http.Request) {
-	t := templateloader.GetTemplate()
-
-	t.ExecuteTemplate(w, "homepage.view.html", struct{}{})
 }
